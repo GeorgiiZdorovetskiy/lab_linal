a10486e1b32303dc976e44d9a5fbbbb7cb12e61c
Георгий <zdorgeoo@mail.ru>
2025-11-12 13:05:04 +0300

    Update task7.py

--- normal/task7.py
+++ normal/task7.py
@@ -4,11 +4,8 @@ def order_and_primitivity_of_t(N: int) -> dict:
     """
     Находит порядок элемента t в группе Z_p^* и проверяет, является ли t образующим (примитивным корнем)
     """
-    p_values = [29, 31, 37, 23, 19]
-    t_values = [9, 8, 7, 12, 14]
-    
-    p = p_values[N % 5]
-    t = t_values[N % 5]
+    p = 29  # p для N mod 5 = 0
+    t = 9   # t для N mod 5 = 0
     
     order = 1
     current = t

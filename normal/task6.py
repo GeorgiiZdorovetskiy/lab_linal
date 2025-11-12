f2468c91c282c8f7a2b1075449126776c56ebab1
Георгий <zdorgeoo@mail.ru>
2025-11-12 13:04:49 +0300

    Update task6.py

--- normal/task6.py
+++ normal/task6.py
@@ -4,13 +4,9 @@ def order_of_sr(N: int) -> int:
     """
     Находит порядок элемента s^r в группе Z_p^*
     """
-    p_values = [29, 31, 37, 23, 19]
-    r_values = [59, 60, 38, 45, 44]
-    s_values = [5, 4, 3, 17, 15]
-    
-    p = p_values[N % 5]
-    r = r_values[N % 5]
-    s = s_values[N % 5]
+    p = 29  # p для N mod 5 = 0
+    r = 59  # r для N mod 5 = 0
+    s = 5   # s для N mod 5 = 0
     
     s_r = pow(s, r, p)
     
